58d4e8a3478328912642ecee7f6190d1406c548b
s3ich4n <s3ich4n@gmail.com>
2024-07-08 23:24:58 +0900

    feat: 프론트 컨트롤러 도입
    request의 내용을 빼서 분기처리

--- src/main/java/com/s3ich4n/hellospring/MainApplication.java
+++ src/main/java/com/s3ich4n/hellospring/MainApplication.java
@@ -26,14 +26,25 @@ public class MainApplication {
                 // 웹 요청의 응답을 세팅함. 뭘 만드는지 알아야 한다는 뜻임.
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    String name = req.getParameter("name");
+                    // 공통기능 처리가 여기에 있음
+                    // 서블릿 컨테이너의 mapping 기능을 Front Controller 가 해야함
+                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals("GET")) {
+                        String name = req.getParameter("name");
 
-                    resp.setStatus(HttpStatus.OK.value());
-                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                    resp.getWriter().println("Hello " + name);
+                        resp.setStatus(HttpStatus.OK.value());
+                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        resp.getWriter().println("Hello " + name);
+
+                    }
+                    else if (req.getRequestURI().equals("/user")) {
+                        //
+                    }
+                    else {
+                        resp.setStatus(HttpStatus.NOT_FOUND.value());
+                    }
                 }
                 // url 매핑을 함. 익명클래스로 만든걸 매핑하겠다는 뜻
-            }).addMapping("/hello");
+            }).addMapping("/*");
         });
         // 서블릿을 구동시킴
         webServer.start();
