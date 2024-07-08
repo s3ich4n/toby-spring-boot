f8ed07858d3d073ae9cc68dcdca1bfc94379a491
s3ich4n <s3ich4n@gmail.com>
2024-07-09 00:39:48 +0900

    feat: 스프링 컨테이너로 통합
    - TEMPLATE METHOD 패턴을 이용하여 처리
    - 이 중, `onRefresh()` 를 오버라이딩해서 서블릿 컨테이너 상속로직을 속에 둠

--- src/main/java/com/s3ich4n/hellospring/MainApplication.java
+++ src/main/java/com/s3ich4n/hellospring/MainApplication.java
@@ -2,6 +2,7 @@ package com.s3ich4n.hellospring;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
@@ -9,19 +10,25 @@ public class MainApplication {
 
     public static void main(String[] args) {
         // 스프링 컨테이너를 만들고, 등록하고, 사용시작을 준비한다
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();  // 이건 써야됨
+
+                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+
+                WebServer webServer = serverFactory.getWebServer(servletContext -> {
+                    servletContext.addServlet("dispatcherServlet",
+                            new DispatcherServlet(this)
+                    ).addMapping("/*");
+                });
+                webServer.start();
+            }
+        };
         applicationContext.registerBean(HelloController.class);
         applicationContext.registerBean(SimpleHelloService.class);
         applicationContext.refresh();
 
-        TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 
-        WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("dispatcherServlet",
-                    // 웹 애플리케이션 컨텍스트 타입을 줘야함
-                    new DispatcherServlet(applicationContext)
-                    ).addMapping("/*");
-        });
-        webServer.start();
     }
 }
