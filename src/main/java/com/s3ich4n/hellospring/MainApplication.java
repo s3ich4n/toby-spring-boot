a2664269ff6f87facffea31ca54d6a6d4acb12f2
s3ich4n <s3ich4n@gmail.com>
2024-07-08 23:32:39 +0900

    feat: 컨트롤러 매핑과 바인딩 설명
    - 이걸 쓸거다 하는 선택: 매핑
    - 자바 타입으로 (DTO, Java Bean) 형식으로 파라미터를 주는 것: 바인
    - (Spring MVC의 그것은 더 복잡하긴함. 근데 원리는 이런식임)

--- src/main/java/com/s3ich4n/hellospring/MainApplication.java
+++ src/main/java/com/s3ich4n/hellospring/MainApplication.java
@@ -21,6 +21,8 @@ public class MainApplication {
         //  내가 이렇게 작업하는 녀석은 구현해야할 메소드가 한개뿐이라 가능함
         //  템플릿-콜백 패턴?
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            HelloController helloController = new HelloController();
+
             // 어댑터 패턴이 여기 들어감
             servletContext.addServlet("hello", new HttpServlet() {
                 // 웹 요청의 응답을 세팅함. 뭘 만드는지 알아야 한다는 뜻임.
@@ -31,9 +33,14 @@ public class MainApplication {
                     if (req.getRequestURI().equals("/hello") && req.getMethod().equals("GET")) {
                         String name = req.getParameter("name");
 
+                        // 이걸 쓸거다 하는 선택: 매핑
+                        // 자바 타입으로 (DTO, Java Bean) 형식으로 파라미터를 주는 것: 바인딩
+                        // (Spring MVC의 그것은 더 복잡하긴함. 근데 원리는 이런식임)
+                        String ret = helloController.hello(name);
+
                         resp.setStatus(HttpStatus.OK.value());
                         resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                        resp.getWriter().println("Hello " + name);
+                        resp.getWriter().println(ret);
 
                     }
                     else if (req.getRequestURI().equals("/user")) {
