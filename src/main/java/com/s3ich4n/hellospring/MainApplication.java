1b91ecf85f40f0c978c9dbca44d8c28607dc3231
s3ich4n <s3ich4n@gmail.com>
2024-07-09 00:19:24 +0900

    feat: DispatcherServlet의 등장
    서비스가 어쩌고 하는 모든것을 분리하기 위해 등장한 개념임.
    
    실제 매핑, 바인딩은 다음 커밋에...

--- src/main/java/com/s3ich4n/hellospring/MainApplication.java
+++ src/main/java/com/s3ich4n/hellospring/MainApplication.java
@@ -1,22 +1,15 @@
 package com.s3ich4n.hellospring;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-
-import java.io.IOException;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class MainApplication {
 
     public static void main(String[] args) {
         // 스프링 컨테이너를 만들고, 등록하고, 사용시작을 준비한다
-        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
         applicationContext.registerBean(HelloController.class);
         applicationContext.registerBean(SimpleHelloService.class);
         applicationContext.refresh();
@@ -24,25 +17,11 @@ public class MainApplication {
         TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("hello", new HttpServlet() {
-                @Override
-                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals("GET")) {
-                        String name = req.getParameter("name");
-
-                        HelloController helloController = applicationContext.getBean(HelloController.class);
-                        String ret = helloController.hello(name);
-
-                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-                        resp.getWriter().println(ret);
-                    }
-                    else {
-                        resp.setStatus(HttpStatus.NOT_FOUND.value());
-                    }
-                }
-            }).addMapping("/*");
+            servletContext.addServlet("dispatcherServlet",
+                    // 웹 애플리케이션 컨텍스트 타입을 줘야함
+                    new DispatcherServlet(applicationContext)
+                    ).addMapping("/*");
         });
         webServer.start();
     }
-
 }
