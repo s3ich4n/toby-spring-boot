52b6a697f0a400995514c55a4fc0f34c64c6f2d8
s3ich4n <s3ich4n@gmail.com>
2024-07-08 23:07:54 +0900

    feat: 서블릿 생성 (1)
    웹 요청의 "응답" 을 세팅함
    
    벌써 패턴만 두개 들어감
    
    - 템플릿-콜백 형식의 구현
    - 어댑터 패턴

--- src/main/java/com/s3ich4n/hellospring/MainApplication.java
+++ src/main/java/com/s3ich4n/hellospring/MainApplication.java
@@ -1,15 +1,40 @@
 package com.s3ich4n.hellospring;
 
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+
+import java.io.IOException;
 
 public class MainApplication {
 
     public static void main(String[] args) {
         TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 
-        // 일관된 방식으로 팩토리에서 꺼내서 추상화 하게하기 위함
-        WebServer webServer = serverFactory.getWebServer();
+        // 람다식으로 전환.
+        //  내가 이렇게 작업하는 녀석은 구현해야할 메소드가 한개뿐이라 가능함
+        //  템플릿-콜백 패턴?
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            // 어댑터 패턴이 여기 들어감
+            servletContext.addServlet("hello", new HttpServlet() {
+                // 웹 요청의 응답을 세팅함. 뭘 만드는지 알아야 한다는 뜻임.
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    resp.setStatus(200);
+                    resp.setHeader("Content-Type", "text/plain");
+                    resp.getWriter().println("Hello Servlet");
+                }
+                // url 매핑을 함. 익명클래스로 만든걸 매핑하겠다는 뜻
+            }).addMapping("/hello");
+        });
+        // 서블릿을 구동시킴
         webServer.start();
     }
 
