d37d87a53197d3d545b586508ce4a792b18eaed8
s3ich4n <s3ich4n@gmail.com>
2024-07-09 01:30:57 +0900

    feat: 다시 스프링의 요소를 사용해보자
    section5 를 준비합시다.

--- src/main/java/com/s3ich4n/hellospring/MainApplication.java
+++ src/main/java/com/s3ich4n/hellospring/MainApplication.java
@@ -23,16 +23,6 @@ public class MainApplication {
     }
 
     public static void main(String[] args) {
-        // 여기 전달받는 클래스의 조건
-        //      1. @Configuration 이 붙은 클래스
-        //      2. 컴포넌트 스캔, Factory 메소드를 가지고 스프링 컨테이너에게
-        //          애플리케이션 구성을 어떻게 할지 알려주는 정보를 갖고있는 클래스
-        //      3. 커맨드라인의 옵션도 받을 수 있어야 함
-        MySpringApplication.run(MainApplication.class, args);
+        SpringApplication.run(MainApplication.class, args);
     }
-
-//    놀랍도록 유사하군.....
-//    public static void main(String[] args) {
-//        SpringApplication.run(MainApplication.class, args);
-//    }
 }
