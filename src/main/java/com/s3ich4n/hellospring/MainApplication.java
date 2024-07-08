aaa2a4ff12da52af868270dd34a44d153a82df57
s3ich4n <s3ich4n@gmail.com>
2024-07-08 22:58:04 +0900

    feat: 톰캣 생성
    웹 팩토리로부터 하나 꺼내와서 구동함

--- src/main/java/com/s3ich4n/hellospring/MainApplication.java
+++ src/main/java/com/s3ich4n/hellospring/MainApplication.java
@@ -1,13 +1,16 @@
 package com.s3ich4n.hellospring;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
 
-@SpringBootApplication
 public class MainApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MainApplication.class, args);
+        TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+
+        // 일관된 방식으로 팩토리에서 꺼내서 추상화 하게하기 위함
+        WebServer webServer = serverFactory.getWebServer();
+        webServer.start();
     }
 
 }
