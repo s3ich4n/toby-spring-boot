9e8f57aae9e67d418f9f627fdb67c432abe7e7ff
s3ich4n <s3ich4n@gmail.com>
2024-07-08 23:54:06 +0900

    feat: 컨테이너 하나 더 추가해서 역할을 분리 (1)
    스프링 컨테이너를 쓰면 훨씬 간단해질걸세...

--- src/main/java/com/s3ich4n/hellospring/HelloController.java
+++ src/main/java/com/s3ich4n/hellospring/HelloController.java
@@ -1,7 +1,11 @@
 package com.s3ich4n.hellospring;
 
+import java.util.Objects;
+
 public class HelloController {
     public String hello(String name) {
-        return "Hello " + name;
+        SimpleHelloService helloService = new SimpleHelloService();
+
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
---
+++ src/main/java/com/s3ich4n/hellospring/SimpleHelloService.java
@@ -0,0 +1,7 @@
+package com.s3ich4n.hellospring;
+
+public class SimpleHelloService {
+    String sayHello(String name) {
+        return "Hello " + name;
+    }
+}
