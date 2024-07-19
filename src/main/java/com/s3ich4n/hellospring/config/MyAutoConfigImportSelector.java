83f84d0aeab3e567ecc3ce9f62cd75fe4a7ff263
s3ich4n <s3ich4n@gmail.com>
2024-07-19 12:34:05 +0900

    feat: 자동 구성정보 파일 분리
    Done
    - META-INF/spring 에 관련 경로를 풀로 넣고 *.import 파일을 넣었다
      - 이제부턴 필요한걸 파일로 관리해서 스프링 부트를 구동하면 된다
    - 위 기능을 하게 해주는 `ClassLoader` 를 임포트해서 썼다

--- src/main/java/com/s3ich4n/hellospring/config/MyAutoConfigImportSelector.java
+++ src/main/java/com/s3ich4n/hellospring/config/MyAutoConfigImportSelector.java
@@ -1,17 +1,38 @@
 package com.s3ich4n.hellospring.config;
 
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
+// BeanClassLoaderAware 를 implement 해서 써도 됨.
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
 
-    // 여기선 Configuration 클래스들을 다 넣고, 후보로 잡아두는 것
-    // 다 쓸건 아니니까
+    // 스프링 부트가 클래스를 로드할 때 쓰는 변수.
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigImportSelector(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[] {
-            "com.s3ich4n.hellospring.config.autoconfig.DispatcherServletConfig",
-            "com.s3ich4n.hellospring.config.autoconfig.TomcatWebServerConfig",
-        };
+        List<String> autoConfigs = new ArrayList<>();
+
+        // for(String candidate : ImportCandidates.load(MyAutoConfiguration.class, classLoader)) {
+        //     autoConfigs.add(candidate);
+        // }
+
+        // 이 annotation 이름과 매칭되는 파일에서 설정값을 읽어옴을 알 수 있다.
+        //      String location = String.format("META-INF/spring/%s.imports", annotation.getName());
+        //       요 로직 보이죠? 왜 썼나 알 수 있음
+        ImportCandidates.load(MyAutoConfiguration.class, classLoader).forEach(autoConfigs::add);
+
+        // return autoConfigs.toArray(String[]::new);
+        // return Arrays.copyOf(autoConfigs.toArray(), autoConfigs.size(), String[].class);
+        return autoConfigs.toArray(new String[0]);      // 이 방법이 가장 대중적
     }
 }
