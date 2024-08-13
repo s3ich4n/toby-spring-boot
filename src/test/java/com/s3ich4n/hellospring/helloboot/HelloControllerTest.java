799d70d341068c4be22f05ecba5715586f88309b
s3ich4n <s3ich4n@gmail.com>
2024-08-13 13:03:56 +0900

    WIP: 이건 숙제!
    이런 경우, 컨트롤러 테스트를 하려면 어떻게 해야하는지 찾기
    안되면 질문하기

--- src/test/java/com/s3ich4n/hellospring/helloboot/HelloControllerTest.java
+++ src/test/java/com/s3ich4n/hellospring/helloboot/HelloControllerTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 public class HelloControllerTest {
     @Test
     void testHello() {
+        // 얘도 마찬가지. 구현해야할 케이스가 여러개니까 안 됨
+        //      이 경우 어떻게 해결하면 좋을지는 내가 알아서 확인해보자!
         HelloController helloController = new HelloController(name -> name);
 
         String ret = helloController.hello("Test");
